311cd8071b73d3e02fa69e93ca71d55dfd5b86b5
Warze <contact@warze.org>
2023-04-28 22:06:54 +0200

    Rewrote leaderboard formatter using map()

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -8,18 +8,17 @@ const leaderboard_emojis = [':first_place:',':second_place:',':third_place:',':f
 
 function cmdLeaderboard(message) {
 	// Get all users (up to 10) with a balance over 0 in descending order
-	db.all('SELECT * FROM users WHERE balance > 0 LIMIT 10 ORDER BY balance DESC', [], (err, results) => {
+	db.all('SELECT * FROM users WHERE balance > 0 ORDER BY balance DESC LIMIT 10 ', [], (err, results) => {
 		if (err) {
 			console.log(err.message);
 			return;
 		}
-		
+
+		// Format the leaderboard
 		let lb_str = '';
-		for (let i = 0; i < Math.min(results.length, 10); i ++) {
-			// Format the string of each user in the leaderboard
-			const user_row = results[i];
-			lb_str += `${leaderboard_emojis[i]} <@${user_row.id}> - **${user_row.balance}** ${emojis.diamond}\n`
-		}
+		results.map((u, i) => {
+			lb_str += `${leaderboard_emojis[i]} <@${u.id}> - **${u.balance}** ${emojis.diamond}\n`
+		})
 		send(message, lb_str);
 	});
 }
