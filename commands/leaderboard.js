3e497d2838c9ef4a7cc53696c713db98dafc4998
Warze <contact@warze.org>
2023-04-28 21:57:14 +0200

    Comments on leaderboard code

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -3,21 +3,25 @@ const { db } = require("../utils/db");
 const { emojis } = require("../utils/emojis");
 const { send } = require("../utils/sender");
 
+// The emojis that appear before the users
 const leaderboard_emojis = [':first_place:',':second_place:',':third_place:',':four:',':five:',':six:',':seven:',':eight:',':nine:',':keycap_ten:']
+
 function cmdLeaderboard(message) {
-	db.all('SELECT * FROM users WHERE balance > 0 ORDER BY balance DESC', [], (err, results) => {
+	// Get all users (up to 10) with a balance over 0 in descending order
+	db.all('SELECT * FROM users WHERE balance > 0 LIMIT 10 ORDER BY balance DESC', [], (err, results) => {
 		if (err) {
 			console.log(err.message);
 			return;
 		}
+		
 		let lb_str = '';
 		for (let i = 0; i < Math.min(results.length, 10); i ++) {
+			// Format the string of each user in the leaderboard
 			const user_row = results[i];
 			lb_str += `${leaderboard_emojis[i]} <@${user_row.id}> - **${user_row.balance}** ${emojis.diamond}\n`
 		}
 		send(message, lb_str);
 	});
 }
-
 
 registerCommand(cmdLeaderboard, "Shows the balance leaderboard", ['leaderboard', 'lb', 'kg'], "");
