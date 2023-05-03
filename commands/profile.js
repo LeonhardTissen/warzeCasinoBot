d75d4350672310a4af8d051a2c5d608f67249b49
Warze <contact@warze.org>
2023-05-03 16:59:33 +0200

    Target arg on profile cmd

--- commands/profile.js
+++ commands/profile.js
@@ -1,15 +1,22 @@
 const { registerCommand } = require("../commands");
-const { createRowIfNotExists } = require("../utils/db");
 const { send } = require("../utils/sender");
 const { getStat } = require("../utils/stats");
+const { parseUser } = require("../utils/usertarget");
 
-function profileCommand(message) {
+function profileCommand(message, target) {
+    // Set self as target if not provided
+    target = parseUser(target, message.author.id);
+    if (!target) {
+        send(message, `Invalid target`);
+        return;
+    }
+
 	// This command does nothing except for return this message
-    getStat('casino2lost', message.author.id).then((c2l) => {
-        getStat('casino2won', message.author.id).then((c2w) => {
-            getStat('casino2dlost', message.author.id).then((c2dl) => {
-                getStat('casino2dwon', message.author.id).then((c2dw) => {
-                    send(message, `(WIP) ${message.author.username}#${message.author.discriminator}
+    getStat('casino2lost', target).then((c2l) => {
+        getStat('casino2won', target).then((c2w) => {
+            getStat('casino2dlost', target).then((c2dl) => {
+                getStat('casino2dwon', target).then((c2dw) => {
+                    send(message, `(WIP) <@${target}>
 **Casino 2:**
 - Wins: ${c2w}
 - Losses: ${c2l}
@@ -24,4 +31,4 @@ function profileCommand(message) {
 }
 
 // Register the command as this function with the description "Sample command", aliases ['sample', 'smpl'] and arguments
-registerCommand(profileCommand, "Shows your profile", ['profile', 'p'], "", false, true);
+registerCommand(profileCommand, "Shows your profile", ['profile', 'p'], "[@user?]", false, true);
