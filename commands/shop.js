126ef3b150b93707dde40c70c879d80f3040389c
Warze <contact@warze.org>
2023-05-04 01:51:57 +0200

    Pagination, better colors & user prefix for shop!

--- commands/shop.js
+++ commands/shop.js
@@ -6,8 +6,17 @@ const { assets } = require("../utils/images");
 const { getPrefix } = require('../utils/getprefix');
 const { shop_items } = require("../utils/shopitems");
 
-function shopCommand(message) {
+function shopCommand(message, page) {
     createRowIfNotExists(message.author.id, 'shop');
+
+    // Amount of pages there are
+    const page_amount = Math.ceil(shop_items.length / 5);
+
+    page = parseInt(page);
+    if (!(page >= 1 && page <= page_amount)) {
+        // Default to page 1 if not provided
+        page = 1;
+    }
 
     db.get('SELECT bought FROM shop WHERE id = ?', [message.author.id], (err, row) => {
         if (err) {
@@ -15,40 +24,50 @@ function shopCommand(message) {
             return;
         }
 
-        const owned_items = row.bought.split(',');
+        // Get prefix of the user first
+        getPrefix(message.author.id).then((prefix) => {
+            const owned_items = row.bought.split(',');
 
-        const cvs = new Canvas(300, 72 * shop_items.length);
-        const ctx = cvs.getContext('2d');
-        const space_per_item = 72;
-        let ypos = 0;
+            const cvs = new Canvas(300, 72 * 5 + 30);
+            const ctx = cvs.getContext('2d');
+            const space_per_item = 72;
+            let ypos = 30;
 
-        getPrefix()
-        shop_items.forEach((item) => {
-            // Draw the shop item itself
-            ctx.drawImage(assets[item.shopimg], 0, ypos);
-
-            // Draw the name of the item
-            ctx.fillStyle = 'white';
+            // Draw the title and page number
+            ctx.fillStyle = '#FF8594';
             ctx.font = '24px sansserif';
-            ctx.fillText(item.name, space_per_item + 5, ypos + 24);
-            
-            const user_owns = owned_items.includes(item.id);
+            ctx.fillText(`Shop (Page ${page}/${page_amount})`, 5, 22);
 
-            // Draw the price of the item
-            ctx.fillStyle = '#FF4965';
-            ctx.font = '18px sansserif';
-            ctx.fillText(user_owns ? '(Purchased)' :`Price: ${item.cost} Diamonds`, space_per_item + 5, ypos + 46);
-            
-            // Command to buy the item, or a message showing that you own it
-            ctx.fillStyle = 'white';
-            ctx.font = '14px sansserif';
-            ctx.fillText(user_owns ? `!setcard ${item.id}` : `!buy ${item.id}`, space_per_item + 5, ypos + 65);
-            
-            ypos += space_per_item;
+            const pageOffset = (page - 1) * 5;
+
+            for (let i = pageOffset; i < Math.min(shop_items.length, pageOffset + 5); i ++) {
+                const item = shop_items[i]
+                // Draw the shop item itself
+                ctx.drawImage(assets[item.shopimg], 0, ypos);
+
+                // Draw the name of the item
+                ctx.fillStyle = 'white';
+                ctx.font = '24px sansserif';
+                ctx.fillText(item.name, space_per_item + 5, ypos + 24);
+                
+                const user_owns = owned_items.includes(item.id);
+
+                // Draw the price of the item
+                ctx.fillStyle = (user_owns ? '#9AAABF': '#FF4965');
+                ctx.font = '18px sansserif';
+                ctx.fillText(user_owns ? '(Purchased)' :`Price: ${item.cost} Diamonds`, space_per_item + 5, ypos + 46);
+                
+                // Command to buy the item, or a message showing that you own it
+                ctx.fillStyle = 'white';
+                ctx.font = '14px sansserif';
+                ctx.fillText(user_owns ? `${prefix}setcard ${item.id}` : `${prefix}buy ${item.id}`, space_per_item + 5, ypos + 65);
+                
+                ypos += space_per_item;
+            }
+
+            sendCvs(message, cvs);
         })
-
-        sendCvs(message, cvs);
     })
 }
 
-registerCommand(shopCommand, "Shows the shop", ['shop', 'sh'], "");
+registerCommand(shopCommand, "Shows the shop", ['shop', 'sh'], "[page?]");
