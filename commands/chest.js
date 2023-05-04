c2df063fe72c874eff139d7c65e5257e7780f5b5
Warze <contact@warze.org>
2023-05-04 15:54:29 +0200

    Ability to open more than 1 chest at a time

--- commands/chest.js
+++ commands/chest.js
@@ -6,9 +6,20 @@ const { randomCustomCard, drawCustomCard } = require("../utils/customcard");
 const { randRange } = require("../utils/random");
 const { changeBalance } = require("../utils/currency");
 const { changeRedChests } = require("../utils/changechests");
+const { isNumeric } = require("../utils/numchoice");
 
-function cmdOpenChest(message) {
+function cmdOpenChest(message, amount) {
     createRowIfNotExists(message.author.id, 'redchest');
+
+    // Default is 1 chest
+    let amountNum = 1;
+    if (['all','a'].includes(amount)) {
+        // Open all chests
+        amountNum = 99999;
+    } else if (isNumeric(amount)) {
+        // Open x amount of chests
+        amountNum = parseInt(amount);
+    }
 
     db.get('SELECT redchest FROM redchest WHERE id = ?', [message.author.id], (err, row) => {
         if (err) {
@@ -16,9 +27,17 @@ function cmdOpenChest(message) {
             return;
         }
 
-        // Check if the user has any chests they can unbox
-        if (row.redchest > 0) {
+        // If the amount opened is larger than the max, just open them all
+        amountNum = Math.min(amountNum, row.redchest);
 
+        // Check if the user has any chests they can unbox
+        if (row.redchest <= 0) {
+            send(message, `<@${message.author.id}>, you have no **Red Chests** ${emojis.redchest}`);
+            return;
+        }
+
+        let chestsOpened = 0;
+        const openInterval = setInterval(function() {
             if (Math.random() > 0.5) {
                 // 50% chance of card unboxing
                 const unboxed_card = randomCustomCard();
@@ -47,13 +66,17 @@ function cmdOpenChest(message) {
                 changeBalance(message.author.id, won_amount);
                 send(message, `You unboxed a **Red Chest** ${emojis.redchest} which contained: **${won_amount}** ${emojis.diamond}`);
             }
+            chestsOpened ++;
 
             // Remove one red chest from their inventory
-            changeRedChests(message.author.id, -1)
-        } else {
-            send(message, `<@${message.author.id}>, you have no **Red Chests** ${emojis.redchest}`);
-        }
+            changeRedChests(message.author.id, -1);
+
+            if (chestsOpened >= amountNum) {
+                clearInterval(openInterval);
+            }
+        }, 1500)
+
     });
 }
 
-registerCommand(cmdOpenChest, "Open a Chest", ['chest', 'ch'], "", false, false);
+registerCommand(cmdOpenChest, "Open a Red Chest", ['chest', 'ch'], "[amount|all?]", false, false);
