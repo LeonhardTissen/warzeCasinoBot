8ffc74df8026491db4ac779c1e318a3f719513b8
Warze <contact@warze.org>
2023-05-05 14:56:54 +0200

    !cards became !inventory and shows decks also now

--- commands/inventory.js
+++ commands/inventory.js
@@ -3,17 +3,20 @@ const { db } = require("../utils/db");
 const { send } = require("../utils/sender");
 const { getPrefix } = require("../utils/getprefix");
 
-function cmdShowCards(message) {
+function cmdShowInventory(message) {
 	// This command does nothing except for return this message
-    let final_str = '';
+    let cards_str = '';
+    let decks_str = '';
     db.get('SELECT bought FROM shop WHERE id = ?', [message.author.id], (err, row) => {
         if (err) {
             console.log(err.message);
             return;
         }
 
+        // Order depending on the type of shop item
         if (row) {
-            final_str += row.bought;
+            cards_str += row.bought.split(',').filter((i) => !i.endsWith('deck')).join(',');
+            decks_str += row.bought.split(',').filter((i) => i.endsWith('deck')).join(',');
         }
 
         db.get('SELECT owned FROM customcard WHERE id = ?', [message.author.id], (err, row) => {
@@ -23,24 +26,34 @@ function cmdShowCards(message) {
             }
 
             if (row) {
-                final_str += ',' + row.owned;
+                cards_str += `,${row.owned}`
             }
 
             getPrefix(message.author.id).then((prefix) => {
-                let rendered_string = `<@${message.author.id}> **has the following cards:**\n\n`;
+                let rendered_string = `<@${message.author.id}>**'s Inventory:**\n\n`;
+
+                rendered_string += `__**Cards:**__\n`;
 
                 rendered_string += `*${prefix}setcard* **normal**\n`;
-                final_str.split(',').forEach((cardid) => {
+                cards_str.split(',').forEach((cardid) => {
                     if (cardid !== '') {
                         rendered_string += `*${prefix}setcard* **${cardid}**\n`;
+                    }
+                });
+
+                rendered_string += `__**Decks:**__\n`;
+
+                rendered_string += `*${prefix}setdeck* **normal**\n`;
+                decks_str.split(',').forEach((deckid) => {
+                    if (deckid !== '') {
+                        rendered_string += `*${prefix}setdeck* **${deckid}**\n`;
                     }
                 })
                 send(message, rendered_string);
             })
-
         })
     })
 }
 
 // Register the command as this function with the description "Sample command", aliases ['sample', 'smpl'] and arguments
-registerCommand(cmdShowCards, "Shows which cards you own", ['cards'], "", false, false);
+registerCommand(cmdShowInventory, "Shows which cards and decks you own", ['inventory', 'inv', 'i', 'cards'], "", false, false);
