45ecbe66988461dacb7c21fe73850dbc3a39b4de
Warze <contact@warze.org>
2023-05-07 13:50:22 +0200

    New diamond sprite (by Ataraxia)

--- utils/emojis.js
+++ utils/emojis.js
@@ -1,7 +1,7 @@
 // Repository of Discord emojis, can be expanded freely
 
 const emojis = {
-	diamond: '<a:diamond:1100814792769409185>',
+	diamond: '<a:diamond:1104736700648927343>',
 	geize: '<:geize:1100812718673498152>',
 	geizeangry: '<:geizeangry:1100812715695538338>',
 	geizesleep: '<:geizesleep:1100812713074110495>',
