bbe7072a3d65650b14e84cca3ab2c75c8f937d3e
Warze <contact@warze.org>
2023-05-09 17:16:27 +0200

    Remove log

--- utils/getprefix.js
+++ utils/getprefix.js
@@ -3,7 +3,6 @@ const { getSettings } = require("./settings");
 function getPrefix(target) {
     return new Promise((resolve) => {
         getSettings(target).then((s) => {
-            console.log(s.pp)
             resolve(s.pp)
         })
     })
