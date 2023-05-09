0510d0c2d56ea8ed1f179a170ddaf00f518c1c0e
Warze <contact@warze.org>
2023-05-09 21:47:23 +0200

    Handle previewcard error

--- commands/previewcard.js
+++ commands/previewcard.js
@@ -1,8 +1,13 @@
 const { registerCommand } = require("../commands");
 const { drawCustomCard } = require("../utils/customcard");
-const { sendCvs } = require("../utils/sender");
+const { sendCvs, send } = require("../utils/sender");
 
 function cmdPreviewCard(message, code) {
+    // Validate card code
+    if (!code) {
+        send(message, 'No card code provided.');
+        return;
+    }
     const cvs = drawCustomCard(code, true);
     
     sendCvs(message, cvs);
