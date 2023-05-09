29bef5185b68fe92458d53fe0de1d4ba5b070e8f
Warze <contact@warze.org>
2023-05-09 17:58:12 +0200

    Added comment

--- utils/getprefix.js
+++ utils/getprefix.js
@@ -2,6 +2,8 @@ const { getSettings } = require("./settings");
 
 function getPrefix(target) {
     return new Promise((resolve) => {
+        // Retrieve the personal prefix setting,
+        // Will default to the global prefix
         getSettings(target).then((s) => {
             resolve(s.pp)
         })
